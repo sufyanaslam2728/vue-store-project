4ff22d827f7f06095799e10815e49f6e3b029c55
Sufyan <sufyanaslam2728@gmail.com>
2025-04-15 16:49:37 +0500

    added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  ccontent: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
